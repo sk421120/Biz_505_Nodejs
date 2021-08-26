664cfed29975732d3f79a85190e9c227e0079c49
sk421120 <sk421120@naver.com>
2021-08-26 16:57:23 +0900

    fileup

--- nodejs_007/public/js/write.js
+++ nodejs_007/public/js/write.js
@@ -38,16 +38,30 @@ const toolbar = [
   "table",
   "height",
   ["para", ["ul", "ol"]],
-  ["view", ["fullscreen", "help"]],
-  ["insert", ["link"]],
+  ["view", ["fullscreen", "help", "codeview"]],
 ];
 
-$("#b_text").summernote({
-  lang: "ko-KR",
-  toolbar,
-  fontNames: fontNames,
-  fontSizes: fontSizes,
-  placeholder: "본문을 입력하세요",
-  width: "100%",
-  height: "300px",
+// jQuery를 사용하여 summernote 적용하기
+$(function () {
+  $("#b_text").summernote({
+    lang: "ko-KR",
+    toolbar,
+    fontNames: fontNames,
+    fontSizes: fontSizes,
+    placeholder: "본문을 입력하세요",
+    width: "100%",
+    height: "300px",
+    // summernote 사용할때 설정하는 event 핸들러
+    callbacks: {
+      /*
+	  summernote 입력창에 이미지를 drag-and-drop 할때 발생하는 event
+
+	  summernote 입력창에 이미지를 drag
+	  */
+      onImageUpload: function (files) {
+        // console.log(editor);
+        fileUpfetch(files);
+      },
+    },
+  });
 });
