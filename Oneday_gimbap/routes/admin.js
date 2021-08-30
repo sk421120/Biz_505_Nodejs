7cfd18b5aee0c395afae9179e91871d8b92a0b41
박은빈 <sk421120@naver.com>
2021-08-31 02:02:34 +0900

    tryAdmin

--- Oneday_gimbap/routes/admin.js
+++ Oneday_gimbap/routes/admin.js
@@ -1,18 +1,25 @@
 const express = require("express");
+const { render } = require("pug");
 const router = express.Router();
+
+const { tbl_products } = require("../models/index");
 
 router.get("/", function (req, res) {
   res.render("admin", { MSG:"비밀번호를 입력하세요"});
 });
 
 router.post("/", function (req, res) {
+  let ppw = "admingimbab!"
   let pw = req.body.pw
   let msg = "비밀번호가 틀렸습니다."
-  if(pw !== "admingimbab!") {
+  if(pw !== "h") {
     res.render("admin", {MSG:msg})  
   }
   msg = "OK"
-  res.render("/admin/product")
+
+  res.render("product", {PRODUCTS : tbl_products})
 });
+
+
 
 module.exports = router;
